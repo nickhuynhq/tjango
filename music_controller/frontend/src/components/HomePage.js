b6fb11cb0b03e4e3f3439672281bcf9c72f82a3a
Nicholas Huynh <nickhuynhq@gmail.com>
2022-11-12 12:29:22 -0500

    Create page components and routes

---
+++ music_controller/frontend/src/components/HomePage.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import RoomJoinPage from "./RoomJoinPage";
+import CreateRoomPage from "./CreateRoomPage";
+import { BrowserRouter, Route, Routes, Link, Redirect } from "react-router-dom";
+
+export default class HomePage extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <>
+        <BrowserRouter>
+          <Routes>
+            <Route exact path="/" element={<p>This is the home page</p>}>
+            </Route>
+            <Route path="/join" element={<RoomJoinPage />} />
+            <Route path="/create" element={<CreateRoomPage />} />
+          </Routes>
+        </BrowserRouter>
+      </>
+    );
+  }
+}
