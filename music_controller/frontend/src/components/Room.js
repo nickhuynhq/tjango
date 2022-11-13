bca3290234db360f48f7baf54dfeb9f9c1e2c6ff
Nicholas Huynh <nickhuynhq@gmail.com>
2022-11-13 12:30:54 -0500

    Create settings button on Room pageand render settings page functionality

--- music_controller/frontend/src/components/Room.js
+++ music_controller/frontend/src/components/Room.js
@@ -1,21 +1,25 @@
 import React, { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { Grid, Button, Typography } from "@material-ui/core";
+import CreateRoomPage from "./CreateRoomPage";
 
 export default function Room(props) {
   const [votesToSkip, setVotesToSkip] = useState(2);
   const [guestCanPause, setGuestCanPause] = useState(false);
   const [isHost, setIsHost] = useState(false);
+  const [showSettings, setShowSettings] = useState(false);
 
   const { roomCode } = useParams();
   const navigate = useNavigate();
 
   const getRoomDetails = () => {
     fetch(`/api/get-room?code=${roomCode}`)
-      .then((response) => { if (!response.ok) {
-        navigate("/")
-      }
-      return response.json()})
+      .then((response) => {
+        if (!response.ok) {
+          navigate("/");
+        }
+        return response.json();
+      })
       .then((data) => {
         setVotesToSkip(data.votes_to_skip);
         setGuestCanPause(data.guest_can_pause);
@@ -29,13 +33,60 @@ export default function Room(props) {
       headers: { "Content-Type": "application/json" },
     };
     fetch("/api/leave-room", requestOptions).then((_response) => {
-      navigate("/")
+      navigate("/");
     });
+  };
+
+  const updateShowSettings = (value) => {
+    setShowSettings(value);
+  };
+
+  const renderSettingsButton = () => {
+    return (
+      <Grid item xs={12}>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => updateShowSettings(true)}
+        >
+          Settings
+        </Button>
+      </Grid>
+    );
+  };
+
+  const renderSettings = () => {
+    return (
+      <Grid container spacing={1} align="center">
+        <Grid item xs={12}>
+          <CreateRoomPage
+            update={true}
+            votesToSkip={votesToSkip}
+            guestCanPause={guestCanPause}
+            roomCode={roomCode}
+            updateCallback={() => {}}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <Button
+            variant="contained"
+            color="secondary"
+            onClick={() => updateShowSettings(false)}
+          >
+            Close
+          </Button>
+        </Grid>
+      </Grid>
+    );
   };
 
   useEffect(() => {
     getRoomDetails();
   }, []);
+
+  if (showSettings) {
+    return renderSettings();
+  }
 
   return (
     <Grid container spacing={1} align="center">
@@ -59,8 +110,13 @@ export default function Room(props) {
           Host: {isHost.toString()}
         </Typography>
       </Grid>
+      {isHost ? renderSettingsButton() : null}
       <Grid item xs={12}>
-        <Button variant="contained" color="secondary" onClick={leaveButtonPressed}>
+        <Button
+          variant="contained"
+          color="secondary"
+          onClick={leaveButtonPressed}
+        >
           Leave Room
         </Button>
       </Grid>
