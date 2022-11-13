aac64e7cce86d755e99a882e8d0356e8faf42853
Nicholas Huynh <nickhuynhq@gmail.com>
2022-11-12 20:06:00 -0500

    Style and format homepage and implement django sessions to redirect to room

--- music_controller/frontend/src/components/HomePage.js
+++ music_controller/frontend/src/components/HomePage.js
@@ -1,30 +1,67 @@
 import React, { Component } from "react";
 import RoomJoinPage from "./RoomJoinPage";
 import CreateRoomPage from "./CreateRoomPage";
+import { Grid, Button, ButtonGroup, Typography } from "@material-ui/core";
 import { BrowserRouter, Route, Routes, Link, Redirect } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 import Room from "./Room";
 
 export default class HomePage extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      roomCode: null,
+    };
   }
 
+  async componentDidMount() {
+    fetch("/api/user-in-room")
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({
+          roomCode: data.code,
+        });
+      });
+  }
+
+  renderHomePage() {
+    if (this.state.roomCode) {
+      return (
+        <Navigate to={`/room/${this.state.roomCode}`} replace={true}/>
+      );
+    } else {
+      return (
+        <Grid container spacing={3}>
+          <Grid item xs={12} align="center">
+            <Typography variant="h3" compact="h3">
+              House Party
+            </Typography>
+          </Grid>
+          <Grid item xs={12} align="center">
+            <ButtonGroup disableElevation variant="contained" color="primary">
+              <Button color="primary" to="/join" component={Link}>
+                Join a Room
+              </Button>
+              <Button color="secondary" to="/create" component={Link}>
+                Create a Room
+              </Button>
+            </ButtonGroup>
+          </Grid>
+        </Grid>
+      );
+    }
+  } 
+
   render() {
-    return (
-      <>
+      return (
         <BrowserRouter>
           <Routes>
-            <Route
-              exact
-              path="/"
-              element={<p>This is the home page</p>}
-            ></Route>
+          <Route exact path="/" element={this.renderHomePage()}/>
             <Route path="/join" element={<RoomJoinPage />} />
             <Route path="/create" element={<CreateRoomPage />} />
-            <Route path="/room/:roomCode" element={<Room />} />
+            <Route path="/room/:roomCode" element={< Room />} /> {/* : in path value represents a parameter in url */ }
           </Routes>
         </BrowserRouter>
-      </>
-    );
+      );
   }
 }
