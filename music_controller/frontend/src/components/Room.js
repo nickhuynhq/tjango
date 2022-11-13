a2d1578f3b14d0be44ef3a4eb10eac83363ce019
Nicholas Huynh <nickhuynhq@gmail.com>
2022-11-12 20:41:34 -0500

    Add leaving room functionality

--- music_controller/frontend/src/components/Room.js
+++ music_controller/frontend/src/components/Room.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
+import { Grid, Button, Typography } from "@material-ui/core";
 
 export default function Room(props) {
   const [votesToSkip, setVotesToSkip] = useState(2);
@@ -7,10 +8,14 @@ export default function Room(props) {
   const [isHost, setIsHost] = useState(false);
 
   const { roomCode } = useParams();
+  const navigate = useNavigate();
 
   const getRoomDetails = () => {
     fetch(`/api/get-room?code=${roomCode}`)
-      .then((response) => response.json())
+      .then((response) => { if (!response.ok) {
+        navigate("/")
+      }
+      return response.json()})
       .then((data) => {
         setVotesToSkip(data.votes_to_skip);
         setGuestCanPause(data.guest_can_pause);
@@ -18,16 +23,47 @@ export default function Room(props) {
       });
   };
 
+  const leaveButtonPressed = () => {
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+    };
+    fetch("/api/leave-room", requestOptions).then((_response) => {
+      navigate("/")
+    });
+  };
+
   useEffect(() => {
     getRoomDetails();
   }, []);
 
   return (
-    <div>
-      <h3>{roomCode}</h3>
-      <p>Votes: {votesToSkip}</p>
-      <p>Guest Can Pause: {guestCanPause.toString()}</p>
-      <p>Host: {isHost.toString()}</p>
-    </div>
+    <Grid container spacing={1} align="center">
+      <Grid item xs={12}>
+        <Typography variant="h4" component="h4">
+          Code: {roomCode}
+        </Typography>
+      </Grid>
+      <Grid item xs={12}>
+        <Typography variant="h6" component="h6">
+          Votes: {votesToSkip}
+        </Typography>
+      </Grid>
+      <Grid item xs={12}>
+        <Typography variant="h6" component="h6">
+          Guest Can Pause: {guestCanPause.toString()}
+        </Typography>
+      </Grid>
+      <Grid item xs={12}>
+        <Typography variant="h6" component="h6">
+          Host: {isHost.toString()}
+        </Typography>
+      </Grid>
+      <Grid item xs={12}>
+        <Button variant="contained" color="secondary" onClick={leaveButtonPressed}>
+          Leave Room
+        </Button>
+      </Grid>
+    </Grid>
   );
 }
