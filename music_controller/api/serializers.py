c33e6348f720827ed5da7cee39945f5779d075f4
Nicholas Huynh <nickhuynhq@gmail.com>
2022-11-13 11:55:21 -0500

    Create UpdateView end point in views

--- music_controller/api/serializers.py
+++ music_controller/api/serializers.py
@@ -13,3 +13,13 @@ class CreateRoomSerializer(serializers.ModelSerializer):
     class Meta:
         model = Room
         fields = ('guest_can_pause', 'votes_to_skip')
+
+class UpdateRoomSerializer(serializers.ModelSerializer):
+
+    # Redefine code field so it does not reference the code from the model
+    # This is because code is unique, and serializer will not work if there is an exisiting code
+    code = serializers.CharField(validators=[])
+    class Meta:
+        model = Room
+        fields = ('guest_can_pause', 'votes_to_skip', 'code')
+
