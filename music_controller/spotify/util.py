649702316d679367ce74be756c078f8189d640b2
Nicholas Huynh <nickhuynhq@gmail.com>
2022-11-13 19:13:07 -0500

    Set up current song view endpoint

--- music_controller/spotify/util.py
+++ music_controller/spotify/util.py
@@ -2,7 +2,7 @@ from .models import SpotifyToken
 from django.utils import timezone
 from datetime import timedelta
 from .credentials import CLIENT_ID, CLIENT_SECRET
-from requests import post
+from requests import post, put, get
 
 BASE_URL = "https://api.spotify.com/v1/me/"
 
@@ -12,6 +12,7 @@ def get_user_tokens(session_id):
         return user_tokens[0]
     else:
         return None
+
 
 def update_or_create_user_tokens(session_id, access_token, token_type, expires_in, refresh_token):
     tokens = get_user_tokens(session_id)
@@ -57,3 +58,21 @@ def refresh_spotify_token(session_id):
 
     update_or_create_user_tokens(
         session_id, access_token, token_type, expires_in, refresh_token)
+
+# Reusable method for executing a Spotify API request
+def execute_spotify_api_request(session_id, endpoint, post_=False, put_=False):
+    tokens = get_user_tokens(session_id)
+    headers = {'Content-Type': 'application/json',
+               'Authorization': "Bearer " + tokens.access_token}
+
+    if post_:
+        post(BASE_URL + endpoint, headers=headers)
+    if put_:
+        put(BASE_URL + endpoint, headers=headers)
+
+    response = get(BASE_URL + endpoint, {}, headers=headers)
+
+    try: 
+        return response.json()
+    except: 
+        return {'Error': 'Issue with request'}
